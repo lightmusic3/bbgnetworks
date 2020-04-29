1834c1532aebee7146a2e79b64de5c9e9e1fd794
lightmusic3 <57882372+lightmusic3@users.noreply.github.com>
2020-04-29 15:27:01 +0900

    index와 test 커밋!

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+</head>
+<?php
+echo "한글"."</br>"."en";
+?>
+<body>
+	<p>dddddddfasdfaga</p>
+</body>
+</html>
